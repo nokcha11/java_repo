ff19c373b4252e17c8126c244e82e06b3865e184
nockcha11 <lie8220@naver.com>
2024-06-19 09:28:50 +0900

    <2024.06.19> - 최초 커밋

---
+++ java_00/src/java_00/HelloJava.java
@@ -0,0 +1,19 @@
+package java_00;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String test1 = "안녕하세요, 저는 000 입니다.";
+
+		System.out.print(test1);
+		
+		System.out.println("Hello Java 2!!!");
+		
+		System.out.println("Hello Java 3!!!");
+		
+		
+	}
+
+}
