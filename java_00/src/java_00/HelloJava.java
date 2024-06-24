ae580d070f76c496b7a8002f75fcfac969606ccd
nockcha11 <lie8220@naver.com>
2024-06-24 14:17:56 +0900

    경고창 뜬느건 봅기싫어 주석처리

--- java_00/src/java_00/HelloJava.java
+++ java_00/src/java_00/HelloJava.java
@@ -4,7 +4,7 @@ package java_00;
 /**
  * 작성자 : 서여진
  * 작성일자 : 2024.06.18
- * *코멘트 : 자바 예제 소스 코드
+ **코멘트 : 자바 예제 소스 코드
  */
 
 public class HelloJava {
@@ -15,11 +15,11 @@ public class HelloJava {
 		
 		String test1 = "안녕하세요, 저는 000 입니다.";
 		
-		String test2 =""; // 안의 내용이 없어서 출력이 안되는 거임
+		//String test2 =""; // 안의 내용이 없어서 출력이 안되는 거임
 		
-		test2 ="나 변수야";
+		//test2 ="나 변수야";
 		
-		String _test = "";
+		//String _test = "";
 
 		System.out.print(test1);
 		//System.out.print(test2); // 출력 안하면 안나온다.
