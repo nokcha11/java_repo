76e8264babca6723d17566f6517677752e37099a
nockcha11 <lie8220@naver.com>
2024-07-03 12:34:04 +0900

    주차관리 프로그램 - 좌석번호 만들기

---
+++ java_00/src/ParkingMng/ParkingController.java
@@ -0,0 +1,13 @@
+package ParkingMng;
+
+public class ParkingController {
+
+	public static void main(String[] args) {
+		
+		ParkingService parkingService = new ParkingService();
+		
+		parkingService.startProgram();
+
+	}
+	
+}
