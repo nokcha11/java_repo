507ce8e096087660f6cf91f0efa4665685d06972
nockcha11 <lie8220@naver.com>
2024-07-03 12:32:40 +0900

    주차관리 프로그램 3가지로 나눠서 만들기

---
+++ java_00/src/Parking/ParkingController.java
@@ -0,0 +1,13 @@
+package Parking;
+
+public class ParkingController {
+
+	public static void main(String[] args) {
+		
+		ParkingService parkingService = new ParkingService();
+		
+		parkingService.startProgram();
+
+	}
+	
+}
