38fd861ee2080105b96c2f8020392a6cb376e3bc
nockcha11 <lie8220@naver.com>
2024-07-05 12:18:36 +0900

    2024.07.05 추상클래스

---
+++ java_00/src/abs03/Exmain.java
@@ -0,0 +1,20 @@
+package abs03;
+
+public class Exmain {
+
+	public class main {
+	    public static void main(String[] args) {
+	        Player player = new Player();
+	        player.play(1);
+
+	        AdvancedLevel aLevel = new AdvancedLevel();
+	        player.upgradeLevel(aLevel);
+	        player.play(2);
+
+	        SuperLevel sLevel = new SuperLevel();
+	        player.upgradeLevel(sLevel);
+	        player.play(3);
+	    }
+	}
+
+}
